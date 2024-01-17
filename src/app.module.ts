78f1662ee38ee62776f7fdd536a21a0e2ecfaa2c
LsR-pop <108394013+LsR-pop@users.noreply.github.com>
2024-01-17 12:07:10 +0100

    app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
